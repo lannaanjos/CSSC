bbf8f1afa699ad37721e0bb15ea442433dd9c4ac
Lanna <lannadosanjoscosta@gmail.com>
2026-03-26 19:56:31 -0300

    meti no cargo

---
+++ Algoritmos_Seguranca/src/AES/aes.rs
@@ -0,0 +1,14 @@
+// expnasao da chave
+
+// add round key
+// é por rodadas
+
+
+// round keys são vetores
+// state é o próprio texto transformar
+
+
+// passa lista de bytes para matriz
+
+// state = matriz da senha
+// vai pegar a round key , vai aplicar xor -em cada byte do state e op bytre correspondente da round key
