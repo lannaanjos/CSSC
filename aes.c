b597d9e50c35988d9547d5b5b6babadf509ff839
LANNA DOS ANJOS COSTA <lanna.dos@cs.up.edu.br>
2026-04-09 20:49:40 -0300

    docs: add xor and explanation

--- aes.c
+++ aes.c
@@ -14,6 +14,8 @@
 há um conjunto [0, 255] que são todos os valores possíveis para um byte*/
 // o elemento "zero" é o próprio 0
 
+// * ops a seguir sem correlação com os as operações aritméticas homonimas
+
 /* /\/\/\/\/\ ADIÇÃO /\/\/\/\/\*/
 
 // há uma operação chamada de adição que se aplica a            |
@@ -24,14 +26,24 @@ há um conjunto [0, 255] que são todos os valores possíveis para um byte*/
 
 /* /\/\/\/\/\ MULTIPLICAÇÃO /\/\/\/\/\*/
 
-// há uma operação q iremos chamar de multiplicação
-// ela é semelhante a adição, mas o elemento "zero"
-// não possui inverso. além disso o elementro neutro
-// é chamado de "um"
-// a multiplicação tbm precisa ser distribuída em 
-// relação à adição
+// há uma operação q iremos chamar de multiplicação             |
+// ela é semelhante a adição, mas o elemento "zero"             |
+// não possui inverso. além disso o elementro neutro            | -> AND
+// é chamado de "um"                                            |
+// a multiplicação tbm precisa ser distribuída em               |
+// relação à adição                                             |
 
-// * sem correlação com os as operações aritméticas homonimas
+// 1. trate cada coperando como um polinônio com base na sua representação binária
+// ex: 6 -> 110 em binário, então vira x²+x
+// 110 ==> 4 + 2 + 0 ==> x² + 2 + 0
+
+// ex: 11 -> 1011 em binário, então vira x³ + x + 1
+// 1011 ==> 8 + 0 + 2 + 1 ==> x³ + x  1
+
+// multiplique-os e divida o resultado por um agente redutor
+// o resto da divisão é o resultado da multiplicação
+
+
 
 
 
