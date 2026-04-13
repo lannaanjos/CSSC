1ca032921f99455ee56352d326c8f83fb85be6e8
Lanna <lannadosanjoscosta@gmail.com>
2026-04-13 15:25:19 -0300

    feat: add consts

--- aes.c
+++ aes.c
@@ -6,10 +6,31 @@
 
 // Ela funciona em rodadas que se repetem conforme o tamanho da chave da entrada.
 
-#include <stdint.h>
+#include <stddef.h>
+#include <stdint.h> // -> uinbt8_t, uint32_t
+#include <string.h> // -> memcpy, memset
+#include <stdio.h>
+#include <stdlib.h>
 
 // CONSTANTES
 
-#define BITS_POR_BYTES 8
+#define TAMANHO_BLOCO_ENTRADA 16 // bytes por bloco
+#define DIMENSAO_ESTADO 4 // porque a matriz do estado é 4x4 bytes
+
+// Nº  de rounds para cada tipo de chave
+// Nk -> n° de palavras de 32 bits na chave original
+#define NK_128 4    // 4 x 32 = 128 bits -> 10 rodadas
+#define NK_192 6   // 6 x 32 = 192 bits -> 12 rodadas
+#define NK_256 8  // 8 x 32 = 256 bits -> 14 rodadas
+
+// Nr -> n° de rodadas  = Nk + 6
+#define NR(nk) ((nk)+6) // macro: Nr a partir do Nk
+
+// Key Schedule produz (Nr + 1) subchaves de 128 bits cada.
+// Máximo = AES-256 -> 14 + 1 = subchaves -> 15 x 16 = 240 bytes
+
+// Constantes MixColumns
+#define GF_POLINIMONIO_IRREDUTIVEL 0x1B
+
 #define TAMANHO_BLOCO_AES 16 // 16 bytes, 128 bits
 #define AES_16_BYTES_KEY 16
