2fea0b419a45120376f5f302615f2ef26f36b95e
Lacci <lannadosanjoscosta@gmail.com>
2026-04-20 17:31:18 -0300

    docs: add pbkdf2 explanation

--- password_vault/pbkdf2.c
+++ password_vault/pbkdf2.c
@@ -1,5 +1,13 @@
-// implementar dps
+
+// pbkdf2-hmac-sha256 -> deriva uma chave AES256 (32 bytes) a partir de uma senha humana.
+//
+// entradas: senha, salt, iterações e comprimento desejado (q vai ser 32 bytes)
+// saída: chave de 32 bytes.
+//
 #include <stdio.h>
+
+
+
 
 int main(){
   return 0;
