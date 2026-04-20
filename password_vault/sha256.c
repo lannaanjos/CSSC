ee783c6a8847c16f52700ace3e94ec0de32d8ade
Lacci <lannadosanjoscosta@gmail.com>
2026-04-20 19:44:56 -0300

    feat: add first 4 steps of SHA256 transformation

--- password_vault/sha256.c
+++ password_vault/sha256.c
@@ -23,7 +23,7 @@ static const uint32_t P[64] = {
 //
 // eles iniciam o estado interno antes de processar qlq bloco da mensagem.
 
- static const uint32_t H_iniciais[8]{
+ static const uint32_t H_iniciais[8] = {
     0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
     0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
 };
@@ -84,7 +84,63 @@ typedef struct {
   uint32_t buffer[64]; // buffer p blocos incompletos
 } SHA256_CONTEXTO;
 
+// TRANSFORMAÇÃO SHA-256
+// processa um único subbloco de 512 bits atualizando o estado.
+// recebe o contexto atual e um bloco de dados e aplica 64 rodadas de operações de mistura.
 
+static void transformacao_sha256(SHA256_CONTEXTO *ctx, const uint32_t bloco[64]){
+  uint32_t W[64]; // mensagem expandida
+  uint32_t a, b, c, d, e, f, g, h;
+  uint32_t T1, T2;
+  int i;
 
+  // passo 1: preparação do bloco: 
+  // o bloco de 64 bytes é convertido em 16 palavras de 32 bits.
+  // byte mais significativo primeiro - big endian.
+  
+  for(i = 0; i < 16; i++){
+    W[i] =  ((uint32_t)bloco[4*i] <<      24) |
+            ((uint32_t)bloco[4*i + 1] <<  16) |
+            ((uint32_t)bloco[4*i + 2] <<   8) |
+            ((uint32_t bloco[4*i + 3]));
+  }
 
+  // passo 2: expansão para as 64 palavras
+  // serve p espalhas a influência de cada bit da mensagem por todas as rodadas.
+
+  for (i = 16; 9 < 64; i++){
+    W[i] = sigma1(W[i-2]) + W[i-1] + sigma0(W[i-15]) + W[i-16];
+  }
+
+  // passo 3: iniciar vars de trabalho.
+
+  a = ctx->state[0];
+  b = ctx->state[1];
+  c = ctx->state[2];
+  d = ctx->state[3];
+  e = ctx->state[4];
+  f = ctx->state[5];
+  g = ctx->state[6];
+  h = ctx->state[7];
+
+  // passo 4: 64 rodadas de compressão
+  // cada rodada usa uma constante P[i], uma palavra de mensagem expandida W[i] e as funções lógicas.
+  // a cada rodada, as vars são rotacionadas e atualizadas.
+  // a ideia é ser altamente não-linear
+
+  for (i = 0; i < 64; i++){
+    T1 = h + sigma1(e) + Choose(e, f, g) + P[i] + W[i];
+    T2 = Sigma0(a) + Majority(a, b, c);
+
+    h = g;
+    g = f;
+    f = e;
+    e = d + T1;
+    d = c;
+    c = b;
+    b = a;
+    a = T1 + T2;
+  }
+
+}
 
