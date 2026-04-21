107b84a42c4da161372239725a512e00d67227a5
Lacci <lannadosanjoscosta@gmail.com>
2026-04-20 22:16:31 -0300

    feat: add hmac files (correctly ts time)

---
+++ password_vault/hmac.c
@@ -0,0 +1,3 @@
+#include "hmac.h"
+#include "sha256.h"
+#include <string.h>
---
+++ password_vault/hmac.h
@@ -0,0 +1,9 @@
+#ifdef HMAC_H
+#define HMAC_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+void hmac_sha255(const uint8_t *chave, size_t tam_chave, const uint8_t mensagem, size_t tam_msg, uint8_t saida[32]);
+
+#endif
