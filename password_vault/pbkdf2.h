4bfbaa11b1eae04a018abdeb24ca4e9d6ac7df54
Lacci <lannadosanjoscosta@gmail.com>
2026-04-20 22:55:47 -0300

    feat: add pbkdf2 header content

---
+++ password_vault/pbkdf2.h
@@ -0,0 +1,12 @@
+#ifndef PBKDF2_H
+#define PBKDF2_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+void pbkdf2_hmac_sha256(const uint8_t *senha, size_t senha,
+                        const uint8_t *salt, size_t tam_salt,
+                        uint32_t iteracoes,
+                        uint8_t *chave_derivada, size_t tam_chave);
+
+#endif
