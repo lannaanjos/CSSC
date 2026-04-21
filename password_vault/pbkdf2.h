2cb961157fa6508fffdf68c23fde8c0a70cdd60e
Lacci <lannadosanjoscosta@gmail.com>
2026-04-21 20:53:48 -0300

    fix: correct argument name

--- password_vault/pbkdf2.h
+++ password_vault/pbkdf2.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <stddef.h>
 
-void pbkdf2_hmac_sha256(const uint8_t *senha, size_t senha,
+void pbkdf2_hmac_sha256(const uint8_t *senha, size_t tam_senha,
                         const uint8_t *salt, size_t tam_salt,
                         uint32_t iteracoes,
                         uint8_t *chave_derivada, size_t tam_chave);
